b8127a1995a86e86d021010d4734703aee08f6fe
atharvdsingh <dsingh9450492600@gmail.com>
2025-11-17 23:11:28 +0530

    my books section

--- src/app/(homeLayout)/my-books/page.tsx
+++ src/app/(homeLayout)/my-books/page.tsx
@@ -4,25 +4,21 @@ import { GetTheSession } from "@/util/GetTheSession";
 import { prisma } from "@/util/Prisma";
 import type { booksHave } from "@prisma/client";
 import { Library } from "lucide-react";
+import { redirect } from "next/navigation";
 import React from "react";
-
 interface Props {}
 
 async function Page(props: Props) {
   const _id=await GetTheSession()
-  if(_id){
+  if(!_id){
+    redirect("/")
+  }
   const books:booksHave[]=await prisma.booksHave.findMany({where:{
-      id:_id.user.id
+      ownerId:_id.user.id
   }})
-  
-
-
-}
-
-
-  return (
-    <>
-      <CenterComponent>
+  console.log(books)
+  if(books.length===0){
+    return <CenterComponent>
         <div className="flex justify-center flex-col gap-3  items-center">
           <Library className=" opacity-50 scale-200" />
           <p className="opacity-50">You haven't posted any books yet</p>
@@ -30,6 +26,17 @@ async function Page(props: Props) {
           <CreateBook />
         </div>
       </CenterComponent>
+  }
+  
+
+
+
+  return (
+    <>
+    {books.map((book)=>(
+      <div className="text-white" key={book.id} >{book.bookname}</div>
+    ))}
+      
     </>
   );
 }
