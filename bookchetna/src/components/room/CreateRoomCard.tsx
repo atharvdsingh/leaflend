87515f243f4e36101a600aedc0331d7c136cf3d8
atharvwork <dev@serpbays.com>
2026-02-21 17:41:04 +0530

    fixed the error

--- bookchetna/src/components/room/CreateRoomCard.tsx
+++ bookchetna/src/components/room/CreateRoomCard.tsx
@@ -53,14 +53,14 @@ function CreateRoomCard() {
           <DialogTrigger className="w-full h-full" asChild>
             <Button
               variant="ghost"
-              className="w-full h-auto py-8 hover:bg-black/50 border-gray-700 border hover:border-white/50 hover:border-2 rounded-2xl transition-all duration-300 flex flex-col items-center justify-center gap-4 bg-gray-900/50 backdrop-blur-sm"
+              className="w-full h-auto py-8 hover:bg-zinc-900 border-zinc-800 border hover:border-zinc-700 rounded-2xl transition-all duration-300 flex flex-col items-center justify-center gap-4 bg-black"
             >
               <div className="flex flex-col gap-3 justify-center items-center">
-                <Plus className="bg-gray-800 size-16 p-3 text-white rounded-full group-hover:scale-110 transition-transform duration-300" />
+                <Plus className="bg-white size-16 p-3 text-black rounded-full group-hover:scale-110 transition-transform duration-300" />
 
                 <div className="text-center">
-                  <p className="text-xl font-semibold mb-1">Create New Room</p>
-                  <p className="text-gray-400 font-normal">
+                  <p className="text-xl text-white font-semibold mb-1">Create New Room</p>
+                  <p className="text-zinc-400 font-normal">
                     Start a new book sharing community
                   </p>
                 </div>
