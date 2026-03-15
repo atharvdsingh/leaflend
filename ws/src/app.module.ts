5a588d6dc063c30fcb50a6db71fd03cf39110495
atharvdsingh <dsingh9450492600@gmail.com>
2026-03-15 22:58:38 +0530

    feat:configure the ws backend

--- ws/src/app.module.ts
+++ ws/src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { RedisModule } from './redis/redis.module';
+import { WebsocketModule } from './websocket/websocket.module';
+import { ConfigModule } from '@nestjs/config';
+import configService from './config/configService';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, RedisModule, WebsocketModule,ConfigModule.forRoot({
+    isGlobal:true,
+    load:[configService]
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
