302b15c4f1fbd8aac54be40f4bcd58f9d25c6e2e
atharvdsingh <dsingh9450492600@gmail.com>
2026-04-15 02:40:13 +0530

    fix: error server can't upgraded to ws from https

--- bookchetna/src/lib/Socket.ts
+++ bookchetna/src/lib/Socket.ts
@@ -1,7 +1,9 @@
-import {io, Socket} from "socket.io-client"
+import { io, Socket } from "socket.io-client"
 import { clientToServer, serverToClient } from "@/types/chat.types";
-const BACKEND_URL= process.env.NEXT_PUBLIC_BACKEND_URL;
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-export const socket:Socket<serverToClient,clientToServer> = io(`${BACKEND_URL}/chat`,{
-    autoConnect:false,reconnection:true
+export const socket: Socket<serverToClient, clientToServer> = io(`${BACKEND_URL}/chat`, {
+    autoConnect: false,
+    reconnection: true,
+    transports: ['websocket']
 });
